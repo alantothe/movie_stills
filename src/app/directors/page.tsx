4adb7b0ae4daca4e0033a76101ea0be6d35b6ba3
alantothe <rubenamalpartida@gmail.com>
2025-04-08 14:06:05 -0400

    css

--- src/app/directors/page.tsx
+++ src/app/directors/page.tsx
@@ -1,7 +1,7 @@
 import DirectorsPage from "@/features/DirectorsPage/DirectorsPage";
 const Directors = () => {
   return (
-    <div className="pt-5">
+    <div className="pt-5 px-10">
       <DirectorsPage />
     </div>
   );
--- src/features/MoviePage/components/AllStills/StillCard.tsx
+++ src/features/MoviePage/components/AllStills/StillCard.tsx
@@ -5,17 +5,15 @@ type StillCardProps = {
 };
 const StillCard = ({ still, id }: StillCardProps) => {
   return (
-    <div >
-
+    <div>
       <Image
-        
-          className=" w-full h-full object-cover cursor-pointer hover:border-amber-300 border-2"
-          alt={id}
-          src={still}
-          data-fancybox="gallery"
-          width={1000}
-          height={1000}
-        ></Image>
+        className=" w-full h-full object-cover cursor-pointer hover:border-amber-300 border-2"
+        alt={id}
+        src={still}
+        data-fancybox="gallery"
+        width={1000}
+        height={1000}
+      ></Image>
     </div>
   );
 };
